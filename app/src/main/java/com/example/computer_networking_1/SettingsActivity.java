eb257ff550550d07af46ed3388621ef0dcfbfbb8
Lawless <vanthuan.cab93@gmail.com>
2019-10-22 00:29:40 +0700

    Setting activity
    task:
    + get id to display
    + update username to database

---
+++ app/src/main/java/com/example/computer_networking_1/SettingsActivity.java
@@ -0,0 +1,28 @@
+package com.example.computer_networking_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SettingsActivity extends AppCompatActivity {
+    private Button UpdateAccountSettings;
+    private EditText userName, userStatus;
+    private CircleImageView userProfileImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        InitializeFields();
+    }
+
+    private void InitializeFields() {
+        UpdateAccountSettings = (Button) findViewById(R.id.update_settings_button);
+        userName = (EditText) findViewById(R.id.username);
+        userStatus = (EditText) findViewById(R.id.set_profile_status);
+        userProfileImage = (CircleImageView) findViewById(R.id.profile_image);
+    }
+}
