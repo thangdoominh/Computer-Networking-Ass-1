5c93c5b6a7deff9fe23b2a1176c2f4ed50a1dd70
tranthambk <48816297+tranthambk@users.noreply.github.com>
2019-10-24 15:42:55 +0700

    test commit file

---
+++ app/src/main/java/com/example/computer_networking_1/GroupChatActivity.java
@@ -0,0 +1,48 @@
+package com.example.computer_networking_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.telephony.mbms.MbmsErrors;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
+
+public class GroupChatActivity extends AppCompatActivity
+{
+    private Toolbar mToolbar;
+    private ImageButton SendMessageButton;
+    private EditText userMessageInput;
+    private ScrollView mScrollView;
+    private TextView displayTextMessages;
+
+    private String currentGroupName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+
+
+
+        currentGroupName = getIntent().getExtras().get("groupName").toString();
+        Toast.makeText(this,currentGroupName,Toast.LENGTH_SHORT).show();
+
+        InitializeFields();
+    }
+
+    private void InitializeFields()
+    {
+        mToolbar = (Toolbar) findViewById(R.id.group_chat_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle(currentGroupName);
+
+        SendMessageButton = (ImageButton) findViewById(R.id.send_message_button);
+        userMessageInput = (EditText) findViewById(R.id.input_group_message);
+        displayTextMessages = (TextView) findViewById(R.id.group_chat_text_display);
+        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
+    }
+}
