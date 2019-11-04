e5d240cfaa849cfb303a190a4a6918850ae4ef4d
Do Minh Thang <dominhthang1999@gmail.com>
2019-11-05 01:26:07 +0700

    Display Chats List
     using Recyclerview in Fragment with Firebase Recycler Adapter

--- app/src/main/java/com/example/computer_networking_1/ChatsFragment.java
+++ app/src/main/java/com/example/computer_networking_1/ChatsFragment.java
@@ -3,11 +3,28 @@ package com.example.computer_networking_1;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 /**
@@ -15,6 +32,11 @@ import android.view.ViewGroup;
  */
 public class ChatsFragment extends Fragment {
 
+    private View PrivateChatView;
+    private RecyclerView chatsList;
+    private DatabaseReference ChatsRef, UserRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
 
     public ChatsFragment() {
         // Required empty public constructor
@@ -24,8 +46,94 @@ public class ChatsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+
+        PrivateChatView = inflater.inflate(R.layout.fragment_chats, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        currentUserID =  mAuth.getCurrentUser().getUid();
+
+        ChatsRef = FirebaseDatabase.getInstance().getReference().child("Contacts").child(currentUserID);
+
+        UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        chatsList = (RecyclerView) PrivateChatView.findViewById(R.id.chats_list);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+
+        chatsList.setLayoutManager(linearLayoutManager); // ????
+
+
+
+
+        return PrivateChatView;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Contacts> options =
+                new FirebaseRecyclerOptions.Builder<Contacts>()
+                        .setQuery(ChatsRef, Contacts.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<Contacts, ChatsViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Contacts, ChatsViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ChatsViewHolder chatsViewHolder, int position, @NonNull Contacts contacts) {
+
+                        final String userIDs = getRef(position).getKey();
+
+                        UserRef.child(userIDs).addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.hasChild("imageURL")){
+
+                                    final String retImage = dataSnapshot.child("imageURL").getValue().toString();
+                                    Picasso.get().load(retImage).into(chatsViewHolder.profileImage);
+
+                                }
+
+                                final String retName = dataSnapshot.child("username").getValue().toString();
+                                final String retStatus = dataSnapshot.child("status").getValue().toString();
+
+                                chatsViewHolder.userName.setText(retName);
+                                chatsViewHolder.userStatus.setText("Last seen: " + "\n" + "Date " + "Time ");
+
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public ChatsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_display_layout, parent, false);
+                        return new ChatsViewHolder(view);
+                    }
+                };
+        chatsList.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+    public static class ChatsViewHolder extends RecyclerView.ViewHolder
+    {
+        CircleImageView profileImage;
+        TextView userName, userStatus;
+
+        public ChatsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            profileImage = itemView.findViewById(R.id.user_profile_image);
+            userName = itemView.findViewById(R.id.user_profile_name);
+            userStatus = itemView.findViewById(R.id.user_status);
+
+        }
+    }
 }
